f86d3cc6c6d0b05a87e6907039633b4cf6aa89ee
Eirik S. Morland <eirik@morland.no>
2017-10-15 13:38:16 +0200

    This was what i meant to do.

--- bin/_kill-switch.js
+++ bin/_kill-switch.js
@@ -1,6 +1,6 @@
 'use strict'
 const appDir = require('app-root-path')
 const config = require(appDir + '/.kill-switch.json')
-const ks = require('../index')
-const killer = ks(config.secret, config.port, () => {})
-killer.start()
+const util = require('util')
+require('http')
+.get(util.format('http://localhost:%d/%s', config.port, config.secret), (resp) => {})
