36bd751949ae5637a44a028f74ffcb55ac4d860c
Eirik S. Morland <eirik@morland.no>
2017-10-17 19:39:52 +0200

    feat(module): Add autostart command
    Add an autostart command, and test coverage for this.

--- index.js
+++ index.js
@@ -1,30 +1,51 @@
 'use strict'
 const http = require('http')
 const util = require('util')
+const appDir = require('app-root-path')
+const fs = require('fs')
 
 const hostname = '127.0.0.1'
 
-const killer = (secret, port = 3000, callback = null) => {
+const killer = (secret = 'secret', port = 3000, callback = null) => {
+  let config = {
+    secret: secret,
+    port: port
+  }
   let server
-  return {
-    start: () => {
-      server = http.createServer((req, res) => {
-        if (req.url === util.format('/%s', secret)) {
-          res.statusCode = 200
-          res.setHeader('Content-Type', 'text/plain')
-          res.end('Stopping')
-          let err = new Error('Killing process!')
-          // Even if the callback is not defined, this will now throw
-          callback(err)
-          throw err
-        }
-        res.statusCode = 403
+  const startIt = (cb) => {
+    server = http.createServer((req, res) => {
+      if (req.url === util.format('/%s', config.secret)) {
+        res.statusCode = 200
         res.setHeader('Content-Type', 'text/plain')
-        res.end()
-      })
+        res.end('Stopping')
+        let err = new Error('Killing process!')
+        // Even if the callback is not defined, this will now throw
+        cb(err)
+        throw err
+      }
+      res.statusCode = 403
+      res.setHeader('Content-Type', 'text/plain')
+      res.end()
+    })
 
-      server.listen(port, hostname, callback)
+    server.listen(config.port, hostname, cb)
+  }
+  return {
+    autoStart: (callback) => {
+      fs.readFile(appDir + '/.kill-switch.json', (err, res) => {
+        if (err) {
+          return callback(err)
+        }
+        try {
+          config = JSON.parse(res.toString('utf8'))
+          config.callback = callback
+          startIt(callback)
+        } catch (err) {
+          return callback(err)
+        }
+      })
     },
+    start: startIt.bind(null, callback),
     stop: () => {
       server.close()
     }
--- test/test.js
+++ test/test.js
@@ -1,9 +1,22 @@
 /* global it, describe */
 'use strict'
+const proxyquire = require('proxyquire')
 require('should')
 const port = 3000
 const secret = 'secret'
 const request = require('supertest')('http://localhost:' + port)
+
+let fsStub = {
+  readFile: (filename, callback) => {
+    callback(null, "{bad json'")
+  }
+}
+
+let arStub = {
+  toString: () => {
+    return '/etc/non-existing'
+  }
+}
 
 describe('All the things', () => {
   it('Should start a server and return something on a wrong secret', (done) => {
@@ -32,5 +45,49 @@ describe('All the things', () => {
         })
     })
     ks.start()
+  })
+
+  it('Should throw on an empty try of autostart', (done) => {
+    const ks = proxyquire('..', {
+      'app-root-path': arStub
+    })
+    ks().autoStart((err) => {
+      err.message.should.equal("ENOENT: no such file or directory, open '/etc/non-existing/.kill-switch.json'")
+      done()
+    })
+  })
+
+  it('Should throw on a crappy json try of autostart', (done) => {
+    const ks = proxyquire('..', {
+      fs: fsStub
+    })
+    let killer = ks()
+    killer.autoStart((err) => {
+      err.message.should.match(/JSON/)
+      done()
+    })
+  })
+
+  it('Should autostart OK if all is good', (done) => {
+    fsStub.readFile = (filename, callback) => {
+      callback(null, `{"port": ${port}, "secret": "${secret}"}`)
+    }
+    const ks = proxyquire('..', {
+      fs: fsStub
+    })
+    let killer = ks()
+    killer.autoStart((err) => {
+      if (err) {
+        if (err.message !== 'Killing process!') {
+          return done(err)
+        }
+        killer.stop()
+        return done()
+      }
+      request.get('/' + secret)
+      .expect(200)
+      .end(() => {
+      })
+    })
   })
 })
